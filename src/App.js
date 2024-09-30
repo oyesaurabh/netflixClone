a8b2bf379025a50bbf0ef3ed8ff67102b0a5d7fc
oyesaurabh <oye.saurabhyadav@gmail.com>
2024-10-01 01:09:31 +0530

    setup redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { AppContent } from "./routes";
+import { Provider } from "react-redux";
+import appStore from "./hooks/appStore";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
